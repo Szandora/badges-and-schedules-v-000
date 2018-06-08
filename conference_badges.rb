d11f16e6b0cfc7b21080b8fad9c22d16c18f1ff5
Sheila Wittman <szandora789@gmail.com>
2018-06-08 20:38:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,14 @@ def batch_badge_creator(array)
   end
   return "Hello my name is #{name}."
 end
+
+def assign_rooms
+  array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  arary.each do
+    room_number = index += 1 
+    prints "Hello, #{name}! You'll be assigned to room #{room_number}!"
+  end
+  return "Hello, #{name}! You'll be assigned to room #{room_number}!"
+end
+end
+  
