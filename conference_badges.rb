dff1d8ab07f834b4c0824e07e5fe60a19c1bf3c6
Sheila Wittman <szandora789@gmail.com>
2018-06-08 20:30:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,11 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  array.each do |name|
+    prints "Hello, my name is #{name}."
+  end
+  return "Hello my name is #{name}."
+end
